4340259c18716304bc99167e694f104f5e9145b4
ninjascant <cvlt.mao@gmail.com>
2016-09-09 13:59:54 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+class Menu extends React.component {
+
+	render() {
+		return (
+			<ul>
+				<ul>
+					<li></li>
+				</ul>
+				<ul>
+					<li></li>
+				</ul>
+			</ul>
+		)
+	}
+}
+
